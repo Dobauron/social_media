974c8b7199b563cde6a2776b06c416c0f28ff66d
dobromir <safetysafetystrazak@gmail.com>
2022-01-26 07:28:54 +0100

    added register option for new user, commence create profile relation for user

---
+++ account/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django import forms
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Hasło',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('hasła nie są identyczne.')
+        return cd['password2']
